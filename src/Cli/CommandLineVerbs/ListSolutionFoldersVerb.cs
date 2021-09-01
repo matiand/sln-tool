3977c714f8ea8c5f78aee2e334db89f346bc3dd2
itsresool <15248621+itsresool@users.noreply.github.com>
2021-09-01 18:06:53 +0200

    Add command for listing folders in solution

--- src/Cli/CommandLineVerbs/ListSolutionFoldersVerb.cs
+++ src/Cli/CommandLineVerbs/ListSolutionFoldersVerb.cs
@@ -1,18 +1,28 @@
+using System;
 using CommandLine;
 using SolutionFile.Document;
+using SolutionFile.Document.Sections;
 
 namespace Cli.CommandLineVerbs
 {
     [Verb("list", HelpText = "List solution folders")]
     public class ListSolutionFoldersVerb : ISolutionDocumentAction
     {
+        private readonly Guid _slnFolderId = new("2150E333-8FDC-42A3-9474-1A3956D46DE8");
+
         [Option(shortName: 'n', "sln-name",
             HelpText = "Name of solution file (if empty will use first .sln file it founds)")]
         public string SolutionFileName { get; set; }
 
         public void Run(SolutionDocument document)
         {
-            // document.
+            foreach (var section in document.Sections)
+            {
+                if (section is ProjectBodyHeader project && project.ProjectType == _slnFolderId)
+                {
+                    Console.WriteLine(project.Name);
+                }
+            }
         }
     }
 }
