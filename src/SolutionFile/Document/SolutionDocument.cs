74d0729d598e1872d994b44c6109b8163fe213f8
itsresool <15248621+itsresool@users.noreply.github.com>
2021-09-02 13:31:21 +0200

    Automatically create folders when adding them

--- src/SolutionFile/Document/SolutionDocument.cs
+++ src/SolutionFile/Document/SolutionDocument.cs
@@ -24,11 +24,13 @@ namespace SolutionFile.Document
 
         public void AddFileToFolder(string folder, string file)
         {
-            if (!FolderExists(folder)) throw new ArgumentException("Folder could not be found");
+            if (!FolderExists(folder))
+            {
+                AddFolder(folder);
+            }
 
             var itemsSection =
                 (SolutionItems)Sections.First(s => s is SolutionItems sItems && sItems.FolderName == folder);
-
             // Silently ignore existing entries
             itemsSection.Elements.TryAdd(file, file);
         }
@@ -39,7 +41,6 @@ namespace SolutionFile.Document
 
             var itemsSection =
                 (SolutionItems)Sections.First(s => s is SolutionItems sItems && sItems.FolderName == folder);
-
             itemsSection.Elements.Remove(file);
         }
 
@@ -58,6 +59,35 @@ namespace SolutionFile.Document
         private bool FolderExists(string name)
         {
             return Sections.Any(s => s is ProjectBodyHeader project && project.Name == name);
+        }
+
+        private void AddFolder(string folderName)
+        {
+            if (Sections.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot add folder to document, when document is empty");
+            }
+
+            // Empty folder block consists of: ProjectBodyHeader, SolutionItems, EndProjectBody sections
+            var header = new ProjectBodyHeader
+            {
+                Id = Guid.NewGuid(),
+                Name = folderName,
+                // Folder is a virtual construct that groups related files, therefore path
+                // is the same as its name
+                // Maybe in the future we can build them outside of solution root
+                RelativePath = folderName,
+                ProjectType = FolderTypeId,
+            };
+            var folderBlock = new List<IDocumentSection>
+            {
+                header,
+                new SolutionItems(folderName),
+                new EndProjectBody(),
+            };
+
+            var insertIdx = Sections.FindLastIndex(s => s is GlobalBodyHeader);
+            Sections.InsertRange(insertIdx, folderBlock);
         }
     }
 }
